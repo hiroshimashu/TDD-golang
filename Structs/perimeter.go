d350fb9b181dd343d35ce849757af4c1db8760f3
hiroshimashu <hiroshimashu@gmail.com>
2020-04-30 14:59:24 +0900

    Add: implementing structs

--- Structs/perimeter.go
+++ Structs/perimeter.go
@@ -1,10 +1,23 @@
 package perimeter
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(width float64, height float64) float64 {
-	return 40.0
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
 }
--- Structs/prerimeter_test.go
+++ Structs/prerimeter_test.go
@@ -3,11 +3,21 @@ package perimeter
 import "testing"
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, s Shape, want float64) {
-		t.Helper()
-		got := s.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
 	}
 }
