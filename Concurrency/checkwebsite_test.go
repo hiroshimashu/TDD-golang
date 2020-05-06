45b96a92ef50e9ac4dd125515dc465ed18595cdf
hiroshimashu <hiroshimashu@gmail.com>
2020-05-06 20:44:05 +0900

    Add: concurrency

---
+++ Concurrency/checkwebsite_test.go
@@ -0,0 +1,29 @@
+package concurrency
+
+import (
+	"testing"
+	"time"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep((20 * time.Millisecond))
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
